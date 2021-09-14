a4b44bf5d3c1d8ae0c34156682fa6860953efab7
lucasmbrute2 <lucasmbrute614@gmail.com>
2021-09-14 02:32:50 -0300

    pagina add tasj

--- frontend-to-do-list/src/App.js
+++ frontend-to-do-list/src/App.js
@@ -2,9 +2,10 @@ import './App.scss';
 import { Switch , Route } from "react-router-dom";
 import Home from "./pages/Home/Home"
 import ViewTask from './pages/viewTask/viewTask';
+import AddTask from "./pages/addTask/addTask";
 import DeleteTask from './pages/deleteTask/deleteTask';
-import Header from "./components/shared/Header/Header"
-import Footer from "./components/shared/Footer/Footer"
+import Header from "./components/shared/Header/Header";
+import Footer from "./components/shared/Footer/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Switch>
           <Route path="/" exact={true} component={Home}/>
           <Route path="/view/:id" component={ViewTask}/>
+          <Route path="/add" component={AddTask}/>
           <Route path="/delete/:id" component ={DeleteTask}/>
         </Switch>
       <Footer/>
---
+++ frontend-to-do-list/src/pages/addTask/addTask.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./addTask.scss"
+
+const AddTask = ()=>{
+    return(
+        <section>
+            Página de add Task
+        </section>
+    )
+};
+export default AddTask;
