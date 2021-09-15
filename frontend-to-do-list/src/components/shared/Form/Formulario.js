d01e57776767fc71f1971a6d02ffc170bbf0f0e9
lucasmbrute2 <lucasmbrute614@gmail.com>
2021-09-15 01:52:08 -0300

    componente formulario

--- frontend-to-do-list/src/components/shared/Form/Formulario.js
+++ frontend-to-do-list/src/components/shared/Form/Formulario.js
@@ -2,35 +2,52 @@ import React from "react";
 import "./Formulario.scss";
 
 const Formulario = (props)=>{
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        
+    } 
+    
     return(
         <section className="add">
             <h1 className="add-h1">{}</h1>
-            <form className="add-form">
+            <form className="add-form" onSubmit={handleSubmit}>
                 <div className="add-form-group">
-                    <label htmlFor="titulo" className="add-form-group-label"></label>
-                    <input ></input>
+                    <label htmlFor="titulo" className="add-form-group-label">Titulo</label>
+                    <input type ="text"name="titulo" className="add-form-group-input"></input>
                 </div>
                 <div className="add-form-group">
-                    <label className="add-form-group-label"></label>
-                    <input></input>
+                    <label htmlFor="descricao" className="add-form-group-label">Descrição</label>
+                    <input className="add-form" name="descricao"></input>
                 </div>
                 <div className="add-form-group">
-                    <label className="add-form-group-label"></label>
-                    <input></input>
+                    <label htmlFor="prioridade"className="add-form-group-label">Prioridade</label>
+                    <select className="add-form-group-select" name="prioridade">
+                        <option value="alta">Alta</option>
+                        <option value="media">Média</option>
+                        <option value="baixa">Baixa</option>
+                    </select>
                 </div>
                 <div className="add-form-group">
-                    <label className="add-form-group-label"></label>
-                    <input></input>
+                    <label htmlFor="status"className="add-form-group-label">Status</label>
+                    <select className="add-form-group-select" name="status">
+                        <option value="fazer">Fazer</option>
+                        <option value="fazendo">Fazendo</option>
+                        <option value="feito">Feito</option>
+                    </select>
                 </div>
                 <div className="add-form-group">
-                    <label className="add-form-group-label"></label>
-                    <input></input>
+                    <label htmlFor="prazo"className="add-form-group-label">Prazo</label>
+                    <input className="add-form-group-input" name="prazo"></input>
                 </div>
                 <div className="add-form-group">
-                    <label className="add-form-group-label"></label>
-                    <input></input>
+                    <label htmlFor="criacao"className="add-form-group-label">Data de criação</label>
+                    <input className="add-form-group-input" name="criacao"></input>
                 </div>  
 
+                <div className="add-form-buttons">
+                    <button className="add-form-buttons-cancelar">Cancelar</button>
+                    <button type="submit" className="add-form-buttons-salvar">Salvar</button>
+                </div>
             
                       
 
--- frontend-to-do-list/src/pages/Home/Home.js
+++ frontend-to-do-list/src/pages/Home/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Home.scss"
 import TaskList from "../../components/structure/taskList/taskList"
-
+import Formulario from "../../components/shared/Form/Formulario"
 
 const Home =() =>{
     return(
         <section className="Home">
+            <Formulario/>
             <h1>To do List</h1>
             <TaskList/>
         </section>
