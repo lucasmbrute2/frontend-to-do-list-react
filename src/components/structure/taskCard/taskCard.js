58ee7822b3119896b49a71dc3c1121df5a118cc6
Lucas <lucasmbrute614@gmail.com>
2021-09-16 01:16:39 -0300

    estilizando card

--- src/components/structure/taskCard/taskCard.js
+++ src/components/structure/taskCard/taskCard.js
@@ -8,10 +8,11 @@ const TaskCard = ({ tarefa })=>{
             <div className='card-container'>
                 <h2 className='card-container-h2'>{tarefa.titulo}</h2>
                 <span className='card-container-descricao'>{tarefa.descricao}</span>
-                <p className='card-container-prioridade'>{tarefa.prioridade}</p>
-                <p className='card-container-p'>{tarefa.estado}</p>
-                <p className='card-container-p'>{tarefa.prazo}</p>	
-                <p className='card-container-p'>{tarefa.criacao}</p>
+                <div className="linha"></div>
+                <p className='card-container-prioridade'>Prioridade: {tarefa.prioridade}</p>
+                <p className='card-container-p'>Status: {tarefa.estado}</p>
+                <p className='card-container-p'>Prazo: {tarefa.prazo}</p>	
+                <p className='card-container-p'>Criado em {tarefa.criacao}</p>
             </div>
         </Link>
     )
