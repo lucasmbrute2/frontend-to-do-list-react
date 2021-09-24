60ced3b402353f5ed936a37c2692ac44ec9a4d04
Lucas <lucasmbrute614@gmail.com>
2021-09-24 02:18:20 -0300

    header att

--- src/components/shared/Header/Header.js
+++ src/components/shared/Header/Header.js
@@ -1,24 +1,29 @@
 import React from "react";
 import "./Header.scss"
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
 
 const Header = ()=>{
     return(
         <header className="header">
                 <ul className="header-list">
-                    <div className='header-list-div'>
-                        <Link to="/" className="header-list-div-item">
-                            <li>Home</li>
-                        </Link>
-                    </div>
-                    <div className='header-list-div'>
-                        <Link to="/add" className="header-list-div-item">
-                            <li>Adicionar tarefa</li>
-                        </Link>
-                    </div>
+                    <Link to="/" className='header-list-div'>
+                        <div className="header-list-div-item">
+                            <FontAwesomeIcon icon={faHome} className='header-list-div-item-fa'></FontAwesomeIcon>
+                            <li className='header-list-div-item-li'>Home</li>
+                        </div>
+                    </Link>
+                    <Link to="/add" className='header-list-div'>
+                        <div className="header-list-div-item">
+                            <FontAwesomeIcon icon={faEdit} className='header-list-div-item-fa'></FontAwesomeIcon>
+                            <li className='header-list-div-item-li'>Adicionar tarefa</li>
+                        </div>
+                    </Link>
 
+                        
                     
-                
                 </ul>
  
         </header>
